4d5f5d625da1deedce9af9d799006ab95b8ccd4e
YuFengjie97 <2457522528@qq.com>
2023-01-14 19:29:52 +0800

    feat: 分离preload逻辑

--- src/views/flappyBird/index.vue
+++ src/views/flappyBird/index.vue
@@ -6,6 +6,7 @@
 import { onMounted, ref } from 'vue'
 import Phaser from 'phaser'
 import { config } from './config'
+import Preload from './Preload'
 import { getUrl } from '@/utils/index'
 
 const con = ref<HTMLElement>()
@@ -13,14 +14,14 @@ const con = ref<HTMLElement>()
 type numsKey = keyof typeof config.nums.static
 
 onMounted(() => {
-  const {width, height} = config.scene
+  const { width, height } = config.scene
   const game = new Phaser.Game({
     type: Phaser.AUTO,
     parent: con.value,
     width,
     height,
     backgroundColor: 0xff0000,
-    scene: [Scene],
+    scene: [Preload, Scene],
     physics: {
       default: 'arcade',
       arcade: {
@@ -32,10 +33,10 @@ onMounted(() => {
     },
   })
   const { innerWidth, innerHeight } = window
-  if(innerWidth < innerHeight) {
+  if (innerWidth < innerHeight) {
     game.canvas.style.cssText = `width: ${innerWidth}px; height: ${innerHeight}px; margin: 0 auto`
   } else {
-    let w = width / height * innerHeight
+    let w = (width / height) * innerHeight
     game.canvas.style.cssText = `width: ${w}px; height: ${innerHeight}px; margin: 0 auto`
   }
 })
@@ -61,75 +62,9 @@ class Scene extends Phaser.Scene {
   soundWing: Phaser.Sound.BaseSound
   soundPoint: Phaser.Sound.BaseSound
   constructor() {
-    super('flappyBird')
+    super('Game')
   }
-  preload() {
-    this.load.image(config.bg.static, getUrl('flappyBird/sprites/background-day.png'))
-    this.load.spritesheet(
-      config.ground.static,
-      getUrl('flappyBird/sprites/ground-sprite.png'),
-      {
-        frameWidth: 336,
-        frameHeight: 112,
-      }
-    )
-    this.load.image(
-      config.startMenu.static,
-      getUrl('flappyBird/sprites/message-initial.png')
-    )
-    this.load.image(config.gameOver.static, getUrl('flappyBird/sprites/gameover.png'))
-    this.load.image(
-      config.restart.static,
-      getUrl('flappyBird/sprites/restart-button.png')
-    )
-    this.load.image(
-      config.pipe.top.static,
-      getUrl('flappyBird/sprites/pipe-green-top.png')
-    )
-    this.load.image(
-      config.pipe.bottom.static,
-      getUrl('flappyBird/sprites/pipe-green-bottom.png')
-    )
-    this.load.image(config.nums.static.num0, getUrl('flappyBird/sprites/number0.png'))
-    this.load.image(config.nums.static.num1, getUrl('flappyBird/sprites/number1.png'))
-    this.load.image(config.nums.static.num2, getUrl('flappyBird/sprites/number2.png'))
-    this.load.image(config.nums.static.num3, getUrl('flappyBird/sprites/number3.png'))
-    this.load.image(config.nums.static.num4, getUrl('flappyBird/sprites/number4.png'))
-    this.load.image(config.nums.static.num5, getUrl('flappyBird/sprites/number5.png'))
-    this.load.image(config.nums.static.num6, getUrl('flappyBird/sprites/number6.png'))
-    this.load.image(config.nums.static.num7, getUrl('flappyBird/sprites/number7.png'))
-    this.load.image(config.nums.static.num8, getUrl('flappyBird/sprites/number8.png'))
-    this.load.image(config.nums.static.num9, getUrl('flappyBird/sprites/number9.png'))
-    this.load.spritesheet(
-      config.birdRed.static,
-      getUrl('flappyBird/sprites/bird-red-sprite.png'),
-      {
-        frameWidth: 34,
-        frameHeight: 24,
-      }
-    )
-    this.load.audio(config.audio.die, [
-      getUrl('flappyBird/audio/die.ogg'),
-      getUrl('flappyBird/audio/die.wav'),
-    ])
-    this.load.audio(config.audio.hit, [
-      getUrl('flappyBird/audio/hit.ogg'),
-      getUrl('flappyBird/audio/hit.wav'),
-    ])
-    this.load.audio(config.audio.point, [
-      getUrl('flappyBird/audio/point.ogg'),
-      getUrl('flappyBird/audio/point.wav'),
-    ])
-    this.load.audio(config.audio.swoosh, [
-      getUrl('flappyBird/audio/swoosh.ogg'),
-      getUrl('flappyBird/audio/swoosh.wav'),
-    ])
-    this.load.audio(config.audio.wing, [
-      getUrl('flappyBird/audio/wing.ogg'),
-      getUrl('flappyBird/audio/wing.wav'),
-    ])
-    console.log('load down')
-  }
+
   create() {
     const { width, height } = config.scene
     this.bg = this.add
